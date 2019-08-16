bc7d6890c57fc066279ca8b317d5e17683e3e929
chaninkou <michaelkoulove@yahoo.com>
2019-08-15 23:30:08 -0700

    Very good problem. Able to find two sum of the list.

---
+++ src/twoSum/FindTwoSumFunction.java
@@ -0,0 +1,33 @@
+package twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindTwoSumFunction {
+	public int[] twoSum(int[] nums, int target){
+		int[] result = new int[2];
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		for(int i = 0; i < nums.length; i++){
+			
+			int answer = target - nums[i];
+			
+			if(map.containsKey(answer)){
+				// Return the index when the element was stored
+				result[0] = map.get(answer);
+				
+				// Getting the index of the element when it contains the answer
+				result[1] = i;
+				
+				return result;
+			}
+			
+			// Store the key as the element and i as the index
+			map.put(nums[i], i);
+			
+		}
+		
+		return result;
+	}
+}
